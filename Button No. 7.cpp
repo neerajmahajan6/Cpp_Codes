9dc0fb01046c0e2ebb7aba3cc547011a39cfdb2c
neerajmahajan6 <65340538+neerajmahajan6@users.noreply.github.com>
2020-06-01 11:06:21 +0530

    Create Button No. 7.cpp
    A program to find size of int, float, double and char in your system.

---
+++ Button No. 7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    char c;
+    float f;
+    double d;
+    cout<<"\nSize of";
+    cout<<"\nInteger - "<<sizeof(i);
+    cout<<"\nCharacter - "<<sizeof(c);
+    cout<<"\nFloat - "<<sizeof(f);
+    cout<<"\nDouble - "<<sizeof(d);
+    return 0;
+}
