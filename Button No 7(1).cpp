16c52cc8c1ee5d212f1419b69957e3989fcd77c0
neerajmahajan6 <65340538+neerajmahajan6@users.noreply.github.com>
2020-06-01 11:07:47 +0530

    Create Button No 7(1).cpp
    A program to find the product of two numbers entered by the user.

---
+++ Button No 7(1).cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers to display its multiplication - ";
+    cin>>a>>b;
+    cout<<"\nMultiplication = "<<a*b;
+    return 0;
+}
